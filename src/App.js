d79e2fa78aaa4c30638ee97c7feb7cea32c34fb3
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-08-01 21:55:30 +1000

    Formating to two spaces

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
@@ -7,57 +8,59 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 
 class App extends Component {
-	state = {
-		users: [],
-		loading: false,
-		alert: null
-	};
+state = {
+    users: [],
+    loading: false,
+    alert: null 
+  };
 
-	async componentDidMount() {
-		this.setState({ loading: true });
-		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
-				.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		this.setState({ users: res.data, loading: false });
-	}
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
+        .REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
 
-	//Search Github users
-	searchUsers = async (text) => {
-		this.setState({ loading: true });
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env
-				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-		this.setState({ users: res.data.items, loading: false });
-	};
+  //Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env
+        .REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
 
-	//Clear Users from state
-	clearUsers = () => this.setState({ users: [], loading: false });
+  //Clear Users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
-	//Set Alert
-	setAlert = (msg, type) => {
-		this.setState({ alert: { msg, type } });
-		setTimeout(() => this.setState({ alert: null }), 3000);
-	};
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
 
-	render() {
-		const { loading, users } = this.state;
-		return (
-			<div className="App">
-				<Navbar title="GitHub User Finder" />
-				<div className="container">
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						setAlert={this.setAlert}
-						showClear={users.length > 0 ? true : false}
-					/>
-					<Users loading={loading} users={users} />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { loading, users } = this.state;
+    return (
+      <Router>
+        <div className="App">
+          <Navbar title="GitHub User Finder" />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Search
+              searchUsers={this.searchUsers}
+              clearUsers={this.clearUsers}
+              setAlert={this.setAlert}
+              showClear={users.length > 0 ? true : false}
+            />
+            <Users loading={loading} users={users} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 function Navbar({ title }) {
-	return (
-		<nav className="navbar bg-dark">
-			<h1>
-				<i className="fab fa-github" /> {title}
-			</h1>
-			<ul>
-				<li>Home</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <i className="fab fa-github" /> {title}
+      </h1>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
 }
 export default Navbar;
