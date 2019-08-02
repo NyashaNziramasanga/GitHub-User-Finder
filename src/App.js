f4878152332661b63dcd4c2a361bfaacd7e84d29
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-08-02 23:45:15 +1000

    About pages and links

--- src/App.js
+++ src/App.js
@@ -1,66 +1,78 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import About from './components/pages/About';
 import Search from './components/users/Search';
 
 class App extends Component {
-state = {
-    users: [],
-    loading: false,
-    alert: null 
-  };
+	state = {
+		users: [],
+		loading: false,
+		alert: null
+	};
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
-        .REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-  }
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
+				.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data, loading: false });
+	}
 
-  //Search Github users
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env
-        .REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data.items, loading: false });
-  };
+	//Search Github users
+	searchUsers = async (text) => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({ users: res.data.items, loading: false });
+	};
 
-  //Clear Users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
+	//Clear Users from state
+	clearUsers = () => this.setState({ users: [], loading: false });
 
-  //Set Alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
-    setTimeout(() => this.setState({ alert: null }), 3000);
-  };
+	//Set Alert
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => this.setState({ alert: null }), 3000);
+	};
 
-  render() {
-    const { loading, users } = this.state;
-    return (
-      <Router>
-        <div className="App">
-          <Navbar title="GitHub User Finder" />
-          <div className="container">
-            <Alert alert={this.state.alert} />
-            <Search
-              searchUsers={this.searchUsers}
-              clearUsers={this.clearUsers}
-              setAlert={this.setAlert}
-              showClear={users.length > 0 ? true : false}
-            />
-            <Users loading={loading} users={users} />
-          </div>
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		const { loading, users } = this.state;
+		return (
+			<Router>
+				<div className="App">
+					<Navbar title="GitHub User Finder" />
+					<div className="container">
+						<Alert alert={this.state.alert} />
+						<Switch>
+							<Route
+								exact
+								path="/"
+								render={(props) => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											setAlert={this.setAlert}
+											showClear={users.length > 0 ? true : false}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path="/about" component={About} />
+						</Switch>
+					</div>
+				</div>
+			</Router>
+		);
+	}
 }
 export default App;
