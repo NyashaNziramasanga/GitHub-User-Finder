1c6adbb3215e047ac124b8e2f9cea451375c2901
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-08-03 13:06:36 +1000

    Spaces

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar({ title }) {
-	return (
-		<nav className="navbar bg-dark">
-			<h1>
-				<i className="fab fa-github" /> {title}
-			</h1>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <i className="fab fa-github" /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 export default Navbar;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,48 +2,48 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Search extends Component {
-	state = {
-		text: ''
-	};
+  state = {
+    text: ''
+  };
 
-	static propTypes = {
-		searchUsers: PropTypes.func.isRequired,
-		clearUsers: PropTypes.func.isRequired,
-		setAlert: PropTypes.func.isRequired,
-		setClear: PropTypes.bool.isRequired
-	};
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    setAlert: PropTypes.func.isRequired,
+    setClear: PropTypes.bool.isRequired
+  };
 
-	onSubmit = (e) => {
-		e.preventDefault();
-		if (this.state.text === '' || this.state.text === null) {
-			this.props.setAlert('Please enter something', 'danger');
-		} else {
-			this.props.searchUsers(this.state.text);
-			this.setState({ text: '' });
-		}
-	};
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text === '' || this.state.text === null) {
+      this.props.setAlert('Please enter something', 'danger');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
+  };
 
-	onChange = (e) => {
-		this.setState({ text: e.target.value });
-	};
+  onChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
 
-	render() {
-		const { showClear, clearUsers } = this.props;
-		return (
-			<div style={{ marginTop: '20px' }}>
-				<form onSubmit={this.onSubmit} className="forms">
-					<input type="text" name="text" placeholder="Search" value={this.state.text} onChange={this.onChange} />
-					<button type="submit" value="search" className="fas fa-search btn search-btn" />
+  render() {
+    const { showClear, clearUsers } = this.props;
+    return (
+      <div style={{ marginTop: '20px' }}>
+        <form onSubmit={this.onSubmit} className="forms">
+          <input type="text" name="text" placeholder="Search" value={this.state.text} onChange={this.onChange} />
+          <button type="submit" value="search" className="fas fa-search btn search-btn" />
 
-					{showClear && (
-						<button className="btn btn-light btn-block" onClick={clearUsers}>
-							Clear
-						</button>
-					)}
-				</form>
-			</div>
-		);
-	}
+          {showClear && (
+            <button className="btn btn-light btn-block" onClick={clearUsers}>
+              Clear
+            </button>
+          )}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Search;
