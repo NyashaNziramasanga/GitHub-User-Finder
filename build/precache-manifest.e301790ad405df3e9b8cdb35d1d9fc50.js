d4863a40bb030babebb2c707e321a2cb32a948a8
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-03-28 02:52:26 +1030

    Build folder

---
+++ build/precache-manifest.e301790ad405df3e9b8cdb35d1d9fc50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadcd9a1d13a7be53e873aec89fe67ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32197b4f8e32aa253f2e",
+    "url": "/static/css/main.21663a6f.chunk.css"
+  },
+  {
+    "revision": "159482e2bc55e38dc8e3",
+    "url": "/static/js/2.d3e7d7cb.chunk.js"
+  },
+  {
+    "revision": "354b6db2b07d14eab39aa51a9e671a5e",
+    "url": "/static/js/2.d3e7d7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32197b4f8e32aa253f2e",
+    "url": "/static/js/main.17ea6df0.chunk.js"
+  },
+  {
+    "revision": "c7ca1642fec3dd4859a3",
+    "url": "/static/js/runtime-main.9b080a5c.js"
+  },
+  {
+    "revision": "9b16f8b8bdfb721b2ee03809351159bf",
+    "url": "/static/media/spinner.9b16f8b8.gif"
+  }
+]);
