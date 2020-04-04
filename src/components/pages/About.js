af16e1d1ebbb8065b86c605b605ed63403872cf6
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-04 23:40:46 +1030

    Refactored About urls

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,12 @@
 import React, { Fragment } from 'react';
 
 const About = () => {
+  const url = {
+    GITHUB_USER_FINDER_URL:
+      'https://github.com/NyashaNziramasanga/GitHub-User-Finder',
+    REACTJS_URL: 'https://reactjs.org/',
+  };
+
   return (
     <Fragment>
       <div className="card">
@@ -13,12 +19,17 @@ const About = () => {
             <a
               target="_blank"
               rel="noreferrer noopener"
-              href="https://github.com/NyashaNziramasanga/GitHub-User-Finder"
+              href={url.GITHUB_USER_FINDER_URL}
             >
               Github Repo
             </a>
           </p>
-          <p>Built with React.js</p>
+          <p>
+            Built with
+            <a target="_blank" rel="noreferrer noopener" href={url.REACTJS_URL}>
+              React.js
+            </a>
+          </p>
         </div>
       </div>
     </Fragment>
