1331f8ea83b255dcacbef6bcaf13f6f18ca71eca
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-04-04 23:35:54 +1030

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,24 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About This App</h1>
-      <p>App to search GitHub Users</p>
-      <p>Version 1.0.0</p>
+      <div className="card">
+        <div className="text-center">
+          <h1>About This App</h1>
+          <p>App to search GitHub users</p>
+          <p>Version 1.0.2</p>
+          <p>
+            <i className="fab fa-github"></i>{' '}
+            <a
+              target="_blank"
+              rel="noreferrer noopener"
+              href="https://github.com/NyashaNziramasanga/GitHub-User-Finder"
+            >
+              Github Repo
+            </a>
+          </p>
+          <p>Built with React.js</p>
+        </div>
+      </div>
     </Fragment>
   );
 };
